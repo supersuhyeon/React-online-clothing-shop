a25cec5843c4bd988d3a3bdf68658336c61a622d
supersuhyeon <suhyeonkr@gmail.com>
2022-12-10 15:20:41 -0800

    set up the project with react router dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import NewProduct from './pages/NewProduct';
+import ProductDetail from './pages/ProductDetail';
+import Mycart from './pages/Mycart';
+
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <App></App>,
+    errorElement : <NotFound></NotFound>,
+    children: [
+      {index:true, element: <Home></Home>},
+      {path: "/products", element: <AllProducts></AllProducts>},
+      {path: "/products/new", element: <NewProduct></NewProduct>},
+      {path: "/products/:id", element: <ProductDetail></ProductDetail>},
+      {path: "/cart", element:<Mycart></Mycart>}
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}></RouterProvider>
   </React.StrictMode>
 );
 
---
+++ src/pages/AllProducts.jsx
@@ -0,0 +1,5 @@
+export default function AllProducts(){
+    return(
+        <p>AllProducts</p>
+    )
+}
